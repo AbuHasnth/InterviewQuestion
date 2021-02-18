bcc842821280ac1f0298ac9605512bbdbe4173fb
Abu Hasnath <abuhasnatjiku@gmail.com>
2021-02-18 10:57:42 +0600

    adding some difrrence of == and ===

---
+++ doublVstripleEqual.js
@@ -0,0 +1,48 @@
+var first = 2;
+var second = "2";
+// double equal doesnt check the types of varible its just check the value is same or not.
+
+if(first==second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
+//triple equal just check the variable types are same or not.it doesn't check the value..
+if(first===second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
+
+
+//1 and true are the same value so the result will be true
+
+var first = 1;
+var second = true;
+
+
+if(first===second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
+
+//0 and false are the same value so the result will be true. 
+
+var first = 0;
+var second = false;
+
+
+if(first===second){
+    console.log("condition is true");
+}
+
+else{
+    console.log("condition is false");
+}
